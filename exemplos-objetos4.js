754eb649e537330251ae763d24491de48f0945a0
Daniel Luiz <loureirodlg@gmail.com>
2024-01-24 17:52:36 -0300

    resolucao sem usar for

---
+++ exemplos-objetos4.js
@@ -0,0 +1,36 @@
+
+
+const produtosConsumidos = [
+    {
+        nomeDoProduto: "whisky",
+        precounit: 30000,
+        quantidade: 1
+
+    },
+    {
+        nomeDoProduto: "red Bull",
+        precounit: 2500,
+        quantidade: 6
+
+    },
+    {
+        nomeDoProduto: "Agua de Coco",
+        precounit: 1000,
+        quantidade: 5
+
+    }
+]
+
+const pessoa = {
+    nome: "Daniel",
+    idade: 30,
+    produtosConsumidos
+}
+
+let somaTotal =
+    (produtosConsumidos[0].precounit * produtosConsumidos[0].quantidade) +
+    (produtosConsumidos[1].precounit * produtosConsumidos[1].quantidade) +
+    (produtosConsumidos[2].precounit * produtosConsumidos[2].quantidade)
+
+console.log(`O senhor ${pessoa.nome} deve pagar R$${(somaTotal / 100).toFixed(2)}`)
+
