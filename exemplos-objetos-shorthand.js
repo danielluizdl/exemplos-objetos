5a18bcec02c6967b1af31f2dc731a1359098d57c
Daniel Luiz <loureirodlg@gmail.com>
2024-01-24 16:20:01 -0300

    entendendo o shorthand

---
+++ exemplos-objetos-shorthand.js
@@ -0,0 +1,17 @@
+const carro = {
+    marca: "Hyundai",
+    modelo: "HB20",
+    ano: 2020,
+    potencia: 1.0
+};
+
+const minhaAltura = 1.70;
+
+const pessoa = {
+    nome: "Daniel",
+    altura: minhaAltura,
+    idade: 30,
+    peso: 90,
+    carro
+}
+console.log(pessoa)
