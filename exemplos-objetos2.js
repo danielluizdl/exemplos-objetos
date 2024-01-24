4941e94ef5c746dac95a609a0bc07599d58fa29d
Daniel Luiz <loureirodlg@gmail.com>
2024-01-24 17:20:14 -0300

    cartao consumo

---
+++ exemplos-objetos2.js
@@ -0,0 +1,27 @@
+const produtosConsumidos = [
+    {
+        nomeDoProduto: "whisky",
+        precounit: 30000,
+        quantidade: 1
+
+    },
+    {
+        nomeDoProduto: "red Bull",
+        precounit: 2500,
+        quantidade: 6
+
+    },
+    {
+        nomeDoProduto: "Agua de Coco",
+        precounit: 1000,
+        quantidade: 5
+
+    }
+]
+
+const pessoa = {
+    nome: "Daniel",
+    idade: 30,
+    produtosConsumidos
+}
+console.log(pessoa)
